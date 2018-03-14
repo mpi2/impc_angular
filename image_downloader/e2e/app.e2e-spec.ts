f51ec38a5eca72a93d2b731b38ddd905c6ebe625
jwgwarren <warren.jonathan@gmail.com>
2018-03-14 18:09:48 +0000

    with example material selector working

---
+++ image_downloader/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('My App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Hello World!');
+  });
+});
--- image_downloader/src/app/app.module.ts
+++ image_downloader/src/app/app.module.ts
@@ -8,13 +8,16 @@ import { ImageBrowserComponent } from './impc.images.browser.component';
 import { FilterComponent } from './impc.images.filter.component';
 import { ImagesRestService } from './impc.images.rest.service';
 import { ShowThumbnailsComponent } from './impc.images.showthumbnails.component';
+import { SelectOverviewExample } from './select-overview-example';
+import { MatSelectModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent, ImageBrowserComponent, FilterComponent, ShowThumbnailsComponent
+    AppComponent, ImageBrowserComponent, FilterComponent, ShowThumbnailsComponent, SelectOverviewExample
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule, MatSelectModule, BrowserAnimationsModule
   ],
   providers: [ImagesRestService],
   bootstrap: [AppComponent]

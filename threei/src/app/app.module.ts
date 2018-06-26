3c7446ef9d02179413c28b51a65f52047fea76a4
jwarren <warren.jonathan@gmail.com>
2018-06-26 15:50:38 +0100

    heatmap working as rough template

--- threei/src/app/app.module.ts
+++ threei/src/app/app.module.ts
@@ -42,7 +42,7 @@ import { HeatmapComponent } from './heatmap/heatmap.component';
 export class AppModule {
   // Diagnostic only: inspect router configuration
   constructor(router: Router) {
-    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+    //console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
   }
 
  }

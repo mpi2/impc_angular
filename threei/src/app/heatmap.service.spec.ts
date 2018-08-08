e8eb3fb3518ff4a45a7d9ec6f2f694e6b1154313
meeniga <amrutha@ebi.ac.uk>
2018-08-08 10:55:06 +0100

    updating

--- threei/src/app/heatmap.service.spec.ts
+++ threei/src/app/heatmap.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { HeatmapService } from './heatmap.service';
+import { HeatmapService } from '../app/heatmap.service';
 
 describe('HeatmapService', () => {
   beforeEach(() => {
---
+++ threei/src/app/procedure-heatmap/procedure-heatmap.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'threei-procedure-heatmap',
+  templateUrl: './procedure-heatmap.component.html',
+  styleUrls: ['./procedure-heatmap.component.css']
+})
+export class ProcedureHeatmapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
